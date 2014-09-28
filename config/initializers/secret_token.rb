b3902c49ddbeab887c1480db06ac8e2673bcb777
Gina Sipley <gsipley@gmail.com>
2014-09-28 14:00:39 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blazer1::Application.config.secret_key_base = '3e16897c23d40465786ac8b105c0c31154707f1f62414d6266ab5ad0421d2fb5b07a3680025151345595f29a6c8b47978189faf51bd2aaae1b72267fe5566f61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blazer1::Application.config.session_store :cookie_store, key: '_blazer1_session'
